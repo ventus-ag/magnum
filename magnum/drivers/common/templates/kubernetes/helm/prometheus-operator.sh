66d6ebb008c897b5a39e094692983448a7ab0020
Yury Zhelezko <yzhelezko@gmail.com>
2021-01-08 15:16:01 +0200

    update

--- magnum/drivers/common/templates/kubernetes/helm/prometheus-operator.sh
+++ magnum/drivers/common/templates/kubernetes/helm/prometheus-operator.sh
@@ -15,7 +15,7 @@ if [ "$(echo ${MONITORING_ENABLED} | tr '[:upper:]' '[:lower:]')" = "true" ]; th
     cat << EOF >> ${HELM_CHART_DIR}/requirements.yaml
 - name: ${CHART_NAME}
   version: ${PROMETHEUS_OPERATOR_CHART_TAG}
-  repository: https://charts.helm.sh/stable
+  repository: https://prometheus-community.github.io/helm-charts
 EOF
 
     # Calculate resources needed to run the Prometheus Monitoring Solution
