acc325d1e329f91e930ca3eb2b8592f8dcd2ebdd
Yury Zhelezko <yzhelezko@gmail.com>
2021-01-20 16:05:43 +0200

    update

--- magnum/drivers/common/templates/kubernetes/fragments/enable-cinder-csi-plugin.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/enable-cinder-csi-plugin.sh
@@ -31,7 +31,7 @@ stringData:
 EOF
 
     csi_plugin_path="/srv/magnum/kubernetes/cinder-csi-plugin"
-    csi_plugin_branch="release-1.20"
+    csi_plugin_branch="release-1.19"
     rm -rf ${csi_plugin_path}
     mkdir -p ${csi_plugin_path}
     curl -L https://github.com/kubernetes/cloud-provider-openstack/archive/${csi_plugin_branch}.tar.gz -o ${csi_plugin_path}/${csi_plugin_branch}.tar.gz
@@ -42,8 +42,6 @@ EOF
     helm package ${csi_plugin_path}/cloud-provider-openstack-${csi_plugin_branch}/charts/cinder-csi-plugin -d ${csi_plugin_path}/package
     helm upgrade -i cinder-csi $(ls -d ${csi_plugin_path}/package/*) -n kube-system \
          --set storageClass.delete.isDefault=true \
-         --set secret.enabled=true \
-         --set secret.name=cinder-csi-cloud-config \
          --set csi.plugin.image.tag="${CINDER_CSI_PLUGIN_TAG}" \
          --set csi.attacher.image.tag="${CSI_ATTACHER_TAG}" \
          --set csi.provisioner.image.tag="${CSI_PROVISIONER_TAG}" \
@@ -59,7 +57,30 @@ EOF
          --set csi.plugin.volumeMounts[1].readOnly=true \
          --set csi.plugin.volumes[0].name=cacert \
          --set csi.plugin.volumes[0].hostPath.path=/etc/kubernetes/ca-bundle.crt \
-         --set csi.plugin.volumes[0].hostPath.type=File
-        # --version 1.2.2 \
+         --set csi.plugin.volumes[0].hostPath.type=File \
+         --set csi.plugin.volumes[1].name=cloud-config \
+         --set csi.plugin.volumes[1].secret.secretName=cinder-csi-cloud-config
+# "release-1.20"
+    # helm upgrade -i cinder-csi $(ls -d ${csi_plugin_path}/package/*) -n kube-system \
+    #      --set storageClass.delete.isDefault=true \
+    #      --set secret.enabled=true \
+    #      --set secret.name=cinder-csi-cloud-config \
+    #      --set csi.plugin.image.tag="${CINDER_CSI_PLUGIN_TAG}" \
+    #      --set csi.attacher.image.tag="${CSI_ATTACHER_TAG}" \
+    #      --set csi.provisioner.image.tag="${CSI_PROVISIONER_TAG}" \
+    #      --set csi.snapshotter.image.tag="${CSI_SNAPSHOTTER_TAG}" \
+    #      --set csi.resizer.image.tag="${CSI_RESIZER_TAG}" \
+    #      --set csi.livenessprobe.image.tag="v2.1.0" \
+    #      --set csi.nodeDriverRegistrar.image.tag="${CSI_NODE_DRIVER_REGISTRAR_TAG}" \
+    #      --set csi.plugin.volumeMounts[0].name=cacert \
+    #      --set csi.plugin.volumeMounts[0].mountPath=/etc/kubernetes/ca-bundle.crt \
+    #      --set csi.plugin.volumeMounts[0].readOnly=true \
+    #      --set csi.plugin.volumeMounts[1].name=cloud-config \
+    #      --set csi.plugin.volumeMounts[1].mountPath=/etc/kubernetes/cloud-config \
+    #      --set csi.plugin.volumeMounts[1].subPath=cloud-config \
+    #      --set csi.plugin.volumeMounts[1].readOnly=true \
+    #      --set csi.plugin.volumes[0].name=cacert \
+    #      --set csi.plugin.volumes[0].hostPath.path=/etc/kubernetes/ca-bundle.crt \
+    #      --set csi.plugin.volumes[0].hostPath.type=File
 fi
 printf "Finished running ${step}\n"
