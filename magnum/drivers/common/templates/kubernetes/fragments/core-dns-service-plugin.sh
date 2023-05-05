c90a0751ab9ece747a1dcfa67908eb9a9a1a3024
Yurii Zheliezko <yzhelezko@gmail.com>
2023-05-06 01:01:04 +0300

    add wait for coredns

--- magnum/drivers/common/templates/kubernetes/fragments/core-dns-service-plugin.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/core-dns-service-plugin.sh
@@ -136,6 +136,6 @@ do
 done
 
 helm repo add coredns https://coredns.github.io/helm
-helm upgrade -i coredns coredns/coredns --version 1.22.0 -n kube-system -f ${CORE_DNS_VALUES_YAML}
+helm upgrade -i coredns coredns/coredns --version 1.22.0 -n kube-system -f ${CORE_DNS_VALUES_YAML} --wait
 
 printf "Finished running ${step}\n"
