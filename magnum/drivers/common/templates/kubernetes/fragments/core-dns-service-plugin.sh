c2ede85f4b3f745512734a5ed0aef54ff9836721
Yurii Zheliezko <yzhelezko@gmail.com>
2023-05-06 13:38:23 +0300

    try

--- magnum/drivers/common/templates/kubernetes/fragments/core-dns-service-plugin.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/core-dns-service-plugin.sh
@@ -142,6 +142,7 @@ helm upgrade -i coredns coredns/coredns --version 1.22.0 -n kube-system -f ${COR
 if [ "${CONTAINER_RUNTIME}" = "containerd"  ] ; then
   $ssh_cmd systemctl restart containerd
   $ssh_cmd kubectl rollout restart deployment coredns -n kube-system
+  $ssh_cmd kubectl rollout status deployment coredns -n kube-system
 fi
 
 printf "Finished running ${step}\n"
