51ba7575609cc60e7869a3d9a5debaa3befe5352
Yury Zhelezko <yzhelezko@gmail.com>
2023-06-05 14:57:00 +0300

    test

--- magnum/drivers/common/templates/kubernetes/fragments/write-heat-params-master.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/write-heat-params-master.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "START: write-heat-params"
+set +x
 
 arch=$(uname -m)
 
@@ -20,10 +21,8 @@ version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
 
 if version_gt $(echo "$KUBE_TAG" | cut -c 2-) 1.25; then
     LEAD_NODE_ROLE_NAME="control-plane"
-    echo "control-plane"
 else
     LEAD_NODE_ROLE_NAME="master"
-    echo "master"
 fi
 
 HEAT_PARAMS=/etc/sysconfig/heat-params
