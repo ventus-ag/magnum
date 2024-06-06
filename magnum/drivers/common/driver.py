25e0c7b76bc851a2a0db1ed38b79fb9fd97a49c8
Yurii Zheliezko <yzhelezko@gmail.com>
2024-06-07 01:26:08 +0300

    test

--- magnum/drivers/common/driver.py
+++ magnum/drivers/common/driver.py
@@ -18,7 +18,6 @@ import six
 from oslo_config import cfg
 from pkg_resources import iter_entry_points
 from stevedore import driver
-from wsme import types as wtypes
 
 from magnum.common import exception
 from magnum.objects import cluster_template
@@ -138,13 +137,12 @@ class Driver(object):
 
     @classmethod
     def get_driver_for_cluster(cls, context, cluster):
-        # if (cluster.labels != wtypes.Unset and cluster.labels is not None
-        #         and 'cluster_template_id' in cluster.labels):
-        #     ct_id = cluster.labels['cluster_template_id']
-        # else:
-        #     ct_id = cluster.cluster_template_id
+        if (cluster.labels is not None and 'cluster_template_id' in cluster.labels):
+            ct_id = cluster.labels['cluster_template_id']
+        else:
+            ct_id = cluster.cluster_template_id
 
-        ct = cluster_template.ClusterTemplate.get_by_uuid(context, "b0662830-f702-474e-b08c-a13581a3891e")
+        ct = cluster_template.ClusterTemplate.get_by_uuid(context, ct_id)
 
         return cls.get_driver(ct.server_type, ct.cluster_distro, ct.coe)
 
