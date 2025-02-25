aa364b2f96edb706d67e8a0b7bd0a87ff69b2031
Yurii Zheliezko <yzhelezko@gmail.com>
2025-02-26 00:45:27 +0200

    fix kubectl

--- magnum/drivers/common/templates/kubernetes/fragments/install-clients.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/install-clients.sh
@@ -17,7 +17,7 @@ echo "export HISTCONTROL=ignoredups" >> /etc/bashrc
 
 # Download to temporary files first
 $ssh_cmd curl --retry 5 --retry-delay 10 -L -o /usr/local/bin/kubelet.tmp https://cdn.dl.k8s.io/release/${KUBE_TAG}/bin/linux/${ARCH}/kubelet
-$ssh_cmd curl --retry 5 --retry-delay 10 -L -o /usr/local/bin/kubectl.tmp https://cdn.dl.k8s.io/release/${KUBE_TAG}/bin/linux/${ARCH}/kubelet
+$ssh_cmd curl --retry 5 --retry-delay 10 -L -o /usr/local/bin/kubectl.tmp https://cdn.dl.k8s.io/release/${KUBE_TAG}/bin/linux/${ARCH}/kubectl
 
 # For kubelet
 if ! $ssh_cmd test -f /usr/local/bin/kubelet || ! $ssh_cmd cmp -s /usr/local/bin/kubelet.tmp /usr/local/bin/kubelet; then
