0767f5347ce0bf11b566beb9e31cc03092efeb32
Yury Zhelezko <yzhelezko@gmail.com>
2026-02-28 19:17:04 +0200

    rotate CA enable

--- magnum/api/controllers/v1/certificate.py
+++ magnum/api/controllers/v1/certificate.py
@@ -27,6 +27,16 @@ from magnum.common import policy
 from magnum import objects
 
 
+class ClusterID(wtypes.Base):
+    """API representation of a cluster ID."""
+
+    uuid = types.uuid
+    """Unique UUID for this cluster"""
+
+    def __init__(self, uuid):
+        self.uuid = uuid
+
+
 class Certificate(base.APIBase):
     """API representation of a certificate.
 
@@ -167,7 +177,7 @@ class CertificateController(base.Controller):
                                                          cert_obj)
         return Certificate.convert_with_links(new_cert)
 
-    @expose.expose(None, types.uuid_or_name, status_code=202)
+    @expose.expose(ClusterID, types.uuid_or_name, status_code=202)
     def patch(self, cluster_ident):
         context = pecan.request.context
         cluster = api_utils.get_resource('Cluster', cluster_ident)
@@ -177,3 +187,4 @@ class CertificateController(base.Controller):
             raise exception.NotSupported("Rotating the CA certificate on a "
                                          "non-TLS cluster is not supported")
         pecan.request.rpcapi.rotate_ca_certificate(cluster)
+        return ClusterID(cluster.uuid)
